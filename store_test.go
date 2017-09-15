38c5b09efa1a2d74b62c921b545601f1d5009496
Dale Hui <dhui@users.noreply.github.com>
2017-09-14 17:57:55 -0700

    Add tests for StoreContext() and LoadContext()

--- store_test.go
+++ store_test.go
@@ -1,6 +1,7 @@
 package passhash
 
 import (
+	"context"
 	"testing"
 )
 
@@ -12,10 +13,30 @@ func TestDummyCredentialStoreStore(t *testing.T) {
 	}
 }
 
+func TestDummyCredentialStoreStoreContext(t *testing.T) {
+	store := DummyCredentialStore{}
+	credential := &Credential{}
+	if err := store.StoreContext(context.Background(), credential); err != nil {
+		t.Error("Got error storing credential.", err)
+	}
+}
+
 func TestDummyCredentialStoreLoad(t *testing.T) {
 	store := DummyCredentialStore{}
 	userID := UserID(0)
 	credential, err := store.Load(userID)
+	if err == nil {
+		t.Error("Got error loading credential.", err)
+	}
+	if credential != nil {
+		t.Error("DummyCredentialStore provided credential.", credential)
+	}
+}
+
+func TestDummyCredentialStoreLoadContext(t *testing.T) {
+	store := DummyCredentialStore{}
+	userID := UserID(0)
+	credential, err := store.LoadContext(context.Background(), userID)
 	if err == nil {
 		t.Error("Got error loading credential.", err)
 	}
